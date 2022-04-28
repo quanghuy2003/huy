392de04d9263b5d201a2760bb03858e9db78be84
huy <huyhq2@bap.jp>
2022-04-28 15:03:08 +0700

    hi

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Ú òa :D!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oce cậu',
+    title: 'Cậu thích tớ rồi đúng không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu đã yêu tớ rồi đóa :> ',
+    btnYes: 'Tớ yêu cậu lắmmmmm <3 <3 <3',
+    btnNo: 'Hờ, mơ đi nhaaa',
+    question:'Nói cho tớ biết tại sao cậu yêu tớ điiii <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì cậu cute thông minh và hiểu tớ <3',
+    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'ib với tớ luôn nhé ;)',
+    btnAccept: 'Okiiiii cậu <3',
+    messLink: 'https://www.facebook.com/huy.le.315428/'
+}
